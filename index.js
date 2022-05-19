cf5b248a70446a876dd6a8ec64b161ffa9aaa6c4
Quentin Motte <motte.quentin1@gmail.com>
2022-05-19 16:36:16 +0200

    add some js

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-//
-//MOBILE NAVIGATION
+////
+//DOM ELEMENTS
 const btnBurger = document.querySelector(".hamburger");
 const navMobile = document.querySelector(".mobile-nav");
 const content = document.querySelector("main");
+const navBar = document.querySelector(".navigation");
+//
+//MOBILE NAVIGATION
 btnBurger.addEventListener("click", () => {
     navMobile.classList.toggle("active");
     content.classList.toggle("shadow");
 });
-console.log(content);
+console.log(navBar);
 content.addEventListener("click", () => {
     navMobile.classList.remove("active");
     content.classList.remove("shadow");
 });
 //
 //
+//SCROLL NAVBAR
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+    if (window.scrollY < lastScroll) {
+        navBar.style.top = 0;
+    } else {
+        navBar.style.top = "-170px";
+    }
+    console.log(lastScroll);
+    lastScroll = window.scrollY;
+});
