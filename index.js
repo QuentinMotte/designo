a67d90f6fd7e2813fcc4a4a2f5fcf238ebcbedf3
Quentin Motte <motte.quentin1@gmail.com>
2022-05-23 16:07:53 +0200

    add js for the form

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const navMobile = document.querySelector(".mobile-nav");
 const content = document.querySelector("main");
 const navBar = document.querySelector(".navigation");
 const arrowToTop = document.querySelector("#arrow-to-top");
-const inputs = document.querySelector("#email");
+const email = document.querySelector("#email");
 const warning = document.querySelector(".warning");
-console.log(warning);
+const textArea = document.querySelector("textarea");
+const form = document.querySelector("form");
+const text = document.querySelector(".message-error");
+const pseudoMessage = document.querySelector(".pseudo-message");
+const pseudoInput = document.querySelector("#name");
+const emailError = document.querySelector(".email-error");
+
+console.log(textArea);
 //
 //MOBILE NAVIGATION
 btnBurger.addEventListener("click", () => {
@@ -43,15 +50,44 @@ window.addEventListener("scroll", () => {
 });
 //
 //Email checker
-inputs.addEventListener("input", (e) => {
-    inputs.value;
-    if (!inputs.value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+email.addEventListener("input", (e) => {
+    email.value;
+    if (!email.value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
         warning.style.display = "block";
-        inputs.style.color = "red";
-    } else {
+        emailError.innerHTML = "email not valide";
+        false;
+    } else if (email.value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
         warning.style.display = "none";
-        inputs.style.color = "white";
+        emailError.innerHTML = "";
         true;
+    } else if (email.value.length === 0) {
+        warning.style.display = "none";
+        emailError.innerHTML = "";
     }
-    console.log(inputs.value);
+});
+//
+//
+pseudoInput.addEventListener("input", (e) => {
+    if (
+        pseudoInput.value.length > 0 &&
+        (pseudoInput.value.length < 3 || pseudoInput.value.length > 20)
+    ) {
+        pseudoMessage.innerHTML =
+            "Le pseudo doit faire entre 3 et 20 caractères";
+    } else {
+        pseudoMessage.innerHTML = "";
+    }
+});
+//
+//
+
+textArea.addEventListener("input", (e) => {
+    if (textArea.value.length < 15) {
+        text.innerHTML = "This cant be empty and under 15 letters";
+    } else {
+        text.innerHTML = "";
+    }
+});
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 });
